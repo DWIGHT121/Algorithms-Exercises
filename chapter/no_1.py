797a134fdea5b10065723363a2f0989b959aa30d
Dwight <sajandktm@gmail.com>
2020-08-14 10:07:52 +0545

    Completed

--- chapter/no_1.py
+++ chapter/no_1.py
@@ -1,15 +1,16 @@
-Write a short Python function, is_multiple(n, m), that takes two integer
-values and returns True if n is a multiple of m, that is, n = mi for some
-integer i, and False otherwise'''
+# Write a short Python function, is_multiple(n, m), that takes two integer
+# values and returns True if n is a multiple of m, that is , n = mi for some
+# integer i, and False otherwise
 
 
-# def is_multiple(n, m):
-#     i = n/m
-#     return True if type(i) == "int"
-#     return False if type(i) != "int"
+def is_multiple(n, m):
+    i = n % m
+    if (i == 0):
+        return True
+    if (i != 0):
+        return False
 
 
-a = input("Enter two numbers, separated by space")
-b = (a.split()
-# print(def(b[0], b[1]))
-print(type(b))
+a = input("Enter two numbers,multiplt of \'a\' and \'a\' separated by space")
+b = a.split()
+print(is_multiple(int(b[0]), int(b[1])))
